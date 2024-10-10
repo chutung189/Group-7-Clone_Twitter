91271cc7cd83c20e0d37505fb6db5a6e1e61398a
Chu Tung <22010841@st.phenikaa-uni.edu.vn>
2024-10-10 10:08:15 +0700

    add file

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
+console.log(process.env.MONGO_URI);
+
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
